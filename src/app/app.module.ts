80c8fcf98ab29b99e729a6fd9bb96bcec8ef531e
guillem <guillem.cordoba@gmail.com>
2017-06-24 21:48:13 +0200

    Added angular firebase configuration to app.module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,18 @@ import { StatusBar } from '@ionic-native/status-bar';
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 
+import { AngularFireModule } from 'angularfire2';
+
+// Configuration for angular firebase, taken from the firebase console
+export const firebaseConfig = {
+    apiKey: "AIzaSyAqLixpYaVk_L5kT2iNPbFnXROAN4C8jZA",
+    authDomain: "wheretogo-v0.firebaseapp.com",
+    databaseURL: "https://wheretogo-v0.firebaseio.com",
+    projectId: "wheretogo-v0",
+    storageBucket: "wheretogo-v0.appspot.com",
+    messagingSenderId: "113543433873"
+}
+
 @NgModule({
   declarations: [
     MyApp,
@@ -14,7 +26,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   bootstrap: [IonicApp],
   entryComponents: [
